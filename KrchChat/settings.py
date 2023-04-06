3eaa4f491a8bf58161d5fc7dd777282ae0a665ff
Ali Kemal Tanriverdi <atanriv1@binghamton.edu>
2023-04-06 00:09:06 -0400

    required updates

--- KrchChat/settings.py
+++ KrchChat/settings.py
@@ -54,7 +54,8 @@ try:
         'AWS_ACCESS_KEY_ID': envs("AWS_ACCESS_KEY_ID"),
         'AWS_SECRET_ACCESS_KEY': envs("AWS_SECRET_ACCESS_KEY"),
         'AWS_STORAGE_BUCKET_NAME': envs("AWS_STORAGE_BUCKET_NAME"),
-        'AWS_S3_Region': envs("AWS_S3_Region")
+        'AWS_S3_Region': envs("AWS_S3_Region"),
+        'DEBUG': envs("DEBUG")
     }
 except:
     env = get_environ_vars()
@@ -66,8 +67,7 @@ except:
 SECRET_KEY = env['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = env['DEBUG'] == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -179,10 +179,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
