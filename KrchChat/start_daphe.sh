36cc59c9044edd89e60815d7d68e988f68097ec2
Ali Kemal Tanriverdi <atanriv1@binghamton.edu>
2023-04-07 16:13:24 -0400

    aws updates for sockets

--- KrchChat/start_daphe.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source /opt/python/run/venv/bin/activate && source /opt/python/current/env
-/opt/python/run/venv/bin/daphne -b 0.0.0.0 -p 5000 KrchChat.asgi:channel_layer
--- KrchChat/start_worker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source /opt/python/run/venv/bin/activate && source /opt/python/current/env
-python manage.py runworker
