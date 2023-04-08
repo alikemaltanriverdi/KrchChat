cde609dd1016cce37a7d3dbf18f86f5388f716bd
Ali Kemal Tanriverdi <atanriv1@binghamton.edu>
2023-04-08 02:11:55 -0400

    updates

--- KrchChat/asgi.py
+++ KrchChat/asgi.py
@@ -8,13 +8,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 import krchchat_app.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KrchChat.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     'websocket': AuthMiddlewareStack(
