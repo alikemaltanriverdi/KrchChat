1677c1cdfd04c7aedaa19316254900bba9a6fb1a
dlum1cs <dlum1@Diana>
2023-04-16 11:41:37 -0400

    conv update

--- KrchChat/static/js/conversations.js
+++ KrchChat/static/js/conversations.js
@@ -5,7 +5,12 @@ var i;
 for (i = 0; i < friend_forms.length; ++i) {
 	let friend_form = friend_forms[i];
 	friend_form.onsubmit = (e) => {
-		console.log("Submitted")
+		e.preventDefault();
+		$('body').load("handleFriendClick/",
+		{
+			'friend': friend_form['friend'].value
+		});
+		return false;
 	}
 	friend_form.children.friendbox.onclick = () => {
 		friend_form.requestSubmit();
