05e2ee13d3a9a731bfdd93e94b52e06137c1a14a
dlum1cs <dlum1@Diana>
2023-04-21 19:46:12 -0400

    reverted changes

--- KrchChat/static/js/conversations.js
+++ KrchChat/static/js/conversations.js
@@ -5,12 +5,7 @@ var i;
 for (i = 0; i < friend_forms.length; ++i) {
 	let friend_form = friend_forms[i];
 	friend_form.onsubmit = (e) => {
-		e.preventDefault();
-		$('body').load("handleFriendClick/",
-		{
-			'friend': friend_form['friend'].value
-		});
-		return false;
+		console.log("Submitted")
 	}
 	friend_form.children.friendbox.onclick = () => {
 		friend_form.requestSubmit();
--- krchchat_app/urls.py
+++ krchchat_app/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.messages_page),
-    path('handleFriendClick/', views.handleFriendClick),
 ]
