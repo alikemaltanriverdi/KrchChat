933e1056df79a88463fba5b433f810822a52d41e
cankaturkmen <cturkmen@binghamton.edu>
2023-05-04 18:01:26 -0400

    updates for creating new converstion if IT DOESNT EXITST

--- KrchChat/static/js/conversations.js
+++ KrchChat/static/js/conversations.js
@@ -13,13 +13,3 @@ for (i = 0; i < friend_forms.length; ++i) {
 	// 	friend_form.submit();
 	// };
 }
-// function submitForm() {
-//     var http = new XMLHttpRequest();
-//     http.open("POST", "<<whereverTheFormIsGoing>>", true);
-//     http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-//     var params = "search=" + <<get search value>>; // probably use document.getElementById(...).value
-//     http.send(params);
-//     http.onload = function() {
-//         alert(http.responseText);
-//     }
-// }
